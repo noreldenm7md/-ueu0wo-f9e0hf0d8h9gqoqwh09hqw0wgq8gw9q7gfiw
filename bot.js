34475a88ea419c951c926387b03e5784cdc9ceb9
khsodinwsmcndbuodihnlw <41130044+noreldenm7md@users.noreply.github.com>
2018-07-15 03:38:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,39 +12,6 @@ client.on('message', message => {
 });
 
 
-const Eris = require("eris");
-var bot = new Eris(NDY3NjYxNjY2NTA3ODE2OTYy.Dit3dw.ocx9UufBH3YwA93PQ0FmJQR6q_0);
-var id = "467857579150934027";
-//لا تعدل ولا شي تحت ذا//
-bot.on("ready", () => {
-onstart();
-});
-
-function onstart(){
-
-setInterval(function(){
-
-var currentTime = new Date(),
-hours = currentTime.getHours() + 0 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "صباحاَ";
-if (hours >= 12) {
-suffix = "مساء";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-
-bot.editChannel(id, { name : "●⌠ " + "Time: " + hours + ":" + minutes + " " + suffix + " ⌡●"});
-}, 60000);
-
-}
 
 
 
